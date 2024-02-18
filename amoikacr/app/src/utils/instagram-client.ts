adfff7c37de9d371165a0dc82798ad47f885428a
Angel Echavarria <echavarriasam@gmail.com>
2024-02-18 16:31:26 +0000

    add eslintc config and formatting with prettier

--- amoikacr/app/src/utils/instagram-client.ts
+++ amoikacr/app/src/utils/instagram-client.ts
@@ -1,8 +1,15 @@
-import { Client, GetPageInfoRequest, GetPageMediaRequest } from 'instagram-graph-api';
+import {
+  Client,
+  GetPageInfoRequest,
+  GetPageMediaRequest,
+} from "instagram-graph-api";
 
-  const client: Client = new Client(ACCESS_TOKEN, PAGE_ID);
+const client: Client = new Client(
+  process.env.INSTAGRAM_ACCESS_TOKEN!,
+  process.env.INSTAGRAM_PAGE_ID!
+);
 
-  const pageInfoRequest: GetPageInfoRequest = client.newGetPageInfoRequest();
-  const pageMediaRequest: GetPageMediaRequest = client.newGetPageMediaRequest();
+const pageInfoRequest: GetPageInfoRequest = client.newGetPageInfoRequest();
+const pageMediaRequest: GetPageMediaRequest = client.newGetPageMediaRequest();
 
-export {pageInfoRequest,pageMediaRequest}
+export { pageInfoRequest, pageMediaRequest };
